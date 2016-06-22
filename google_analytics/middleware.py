98f9eaadda046bb674e0e24103168dce7da18dc5
Trevor Cox <trevor.cox@alumni.ubc.ca>
2016-06-22 12:56:04 -0400

    GoogleAnalyticsMiddleware: - use UA not GA. - catch and log exceptions to ensure that middleware doesn't break site.

--- google_analytics/middleware.py
+++ google_analytics/middleware.py
@@ -1,19 +1,28 @@
 from django.conf import settings
-from google_analytics.utils import build_ga_params, set_cookie
+from google_analytics.utils import build_ua_params, set_cookie
 from google_analytics.tasks import send_tracking
 
+import logging
+logger = logging.getLogger(__name__)
 
 class GoogleAnalyticsMiddleware(object):
     def process_response(self, request, response):
-        if hasattr(settings, 'GOOGLE_ANALYTICS_IGNORE_PATH'):
-            exclude = [p for p in settings.GOOGLE_ANALYTICS_IGNORE_PATH
-                       if request.path.startswith(p)]
-            if any(exclude):
-                return response
+        try:
+            logger.debug('GoogleAnalyticsMiddleware')
+            if hasattr(settings, 'GOOGLE_ANALYTICS_IGNORE_PATH'):
+                exclude = [p for p in settings.GOOGLE_ANALYTICS_IGNORE_PATH
+                           if request.path.startswith(p)]
+                if any(exclude):
+                    return response
+    
+            path = request.path
+            referer = request.META.get('HTTP_REFERER', '')
+            params = build_ua_params(request, path=path, referer=referer)
+            response = set_cookie(params, response)
+            send_tracking.delay(params)
+        except Exception as e:
+            # With django-rest-framework we may see:
+            #  AttributeError: 'WSGIRequest' object has no attribute 'session'
+            logger.debug('GoogleAnalyticsMiddleware: %s', e)
 
-        path = request.path
-        referer = request.META.get('HTTP_REFERER', '')
-        params = build_ga_params(request, path=path, referer=referer)
-        response = set_cookie(params, response)
-        send_tracking.delay(params)
         return response
